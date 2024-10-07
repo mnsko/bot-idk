f336bb7fc4aa47fed6d813a0f88fad47bcfdda79
mnsk <skopicek.m@gmail.com>
2024-10-07 19:48:33 +0200

    Fixes that bot spam in ranked_channel Fixes #4

--- l.py
+++ l.py
@@ -85,7 +85,21 @@ def save_last_match(game_name, tag_line, match_id):
     with open(filename, 'w') as f:
         f.write(match_id)
 
-# New function to rank players
+# Load the last LP data for a specific account
+def load_last_lp(game_name, tag_line):
+    filename = f"{game_name}_{tag_line}_lp.txt"
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
+    return {}
+
+# Save the LP data for a specific account
+def save_last_lp(game_name, tag_line, lp_data):
+    filename = f"{game_name}_{tag_line}_lp.txt"
+    with open(filename, 'w') as f:
+        json.dump(lp_data, f)
+
+# Rank players by tier, division, and LP
 def rank_players(league_entries):
     rank_order = {
         'CHALLENGER': 0, 'GRANDMASTER': 1, 'MASTER': 2, 'DIAMOND': 3,
@@ -102,7 +116,7 @@ def rank_players(league_entries):
     
     return sorted(league_entries, key=rank_key)
 
-# New function to format ranked stats
+# Format ranked stats for display
 def format_ranked_stats(ranked_players, queue_type):
     formatted = f"**{queue_type}**\n"
     for player in ranked_players:
@@ -124,71 +138,105 @@ async def update_ranked_stats(channel):
                 league_entries = get_league_entries(encrypted_summoner_id)
                 solo_entry = next((entry for entry in league_entries if entry['queueType'] == 'RANKED_SOLO_5x5'), None)
                 flex_entry = next((entry for entry in league_entries if entry['queueType'] == 'RANKED_FLEX_SR'), None)
+
+                last_lp_data = load_last_lp(game_name, tag_line)
                 
+                solo_lp_changed = False
+                flex_lp_changed = False
+
                 if solo_entry:
                     solo_entry['summonerName'] = f"{game_name}#{tag_line}"
                     all_solo_entries.append(solo_entry)
+                    
+                    # Compare LP for Solo Queue
+                    last_solo_lp = last_lp_data.get('solo', {}).get('lp', None)
+                    if last_solo_lp is not None and last_solo_lp != solo_entry['leaguePoints']:
+                        solo_lp_changed = True
+                        await channel.send(f"**{game_name}#{tag_line}** has {'gained' if solo_entry['leaguePoints'] > last_solo_lp else 'lost'} LP in **Solo Queue**!\n"
+                                           f"Current LP: **{solo_entry['leaguePoints']} LP**")
+                    
+                    last_lp_data['solo'] = {
+                        'lp': solo_entry['leaguePoints']
+                    }
                 else:
-                    all_solo_entries.append(None)  # Add None for unranked players
+                    all_solo_entries.append(None)
 
                 if flex_entry:
                     flex_entry['summonerName'] = f"{game_name}#{tag_line}"
                     all_flex_entries.append(flex_entry)
+                    
+                    # Compare LP for Flex Queue
+                    last_flex_lp = last_lp_data.get('flex', {}).get('lp', None)
+                    if last_flex_lp is not None and last_flex_lp != flex_entry['leaguePoints']:
+                        flex_lp_changed = True
+                        await channel.send(f"**{game_name}#{tag_line}** has {'gained' if flex_entry['leaguePoints'] > last_flex_lp else 'lost'} LP in **Flex Queue**!\n"
+                                           f"Current LP: **{flex_entry['leaguePoints']} LP**")
+                    
+                    last_lp_data['flex'] = {
+                        'lp': flex_entry['leaguePoints']
+                    }
                 else:
-                    all_flex_entries.append(None)  # Add None for unranked players
+                    all_flex_entries.append(None)
+                
+                # Save the updated LP data
+                save_last_lp(game_name, tag_line, last_lp_data)
     
-    ranked_solo = rank_players(all_solo_entries)
-    ranked_flex = rank_players(all_flex_entries)
-    
-    solo_stats = format_ranked_stats(ranked_solo, "Solo/Duo Queue")
-    flex_stats = format_ranked_stats(ranked_flex, "Flex Queue")
-    
-    embed = Embed(title="Ranked Stats", color=discord.Color.blue())
-    embed.add_field(name="Solo/Duo Queue", value=solo_stats, inline=False)
-    embed.add_field(name="Flex Queue", value=flex_stats, inline=False)
-    
-    # Delete previous messages and send the new one
-    async for message in channel.history(limit=None):
-        await message.delete()
-    await channel.send(embed=embed)
+    # Only send the updated ranked stats if there's any LP change
+    if solo_lp_changed or flex_lp_changed:
+        ranked_solo = rank_players(all_solo_entries)
+        ranked_flex = rank_players(all_flex_entries)
+        
+        solo_stats = format_ranked_stats(ranked_solo, "Solo/Duo Queue")
+        flex_stats = format_ranked_stats(ranked_flex, "Flex Queue")
+        
+        embed = Embed(title="Ranked Stats", color=discord.Color.blue())
+        embed.add_field(name="Solo/Duo Queue", value=solo_stats, inline=False)
+        embed.add_field(name="Flex Queue", value=flex_stats, inline=False)
+        
+        # Delete previous messages and send the new one
+        async for message in channel.history(limit=None):
+            await message.delete()
+        await channel.send(embed=embed)
 
 async def check_match(channel, game_name, tag_line):
     puuid = get_puuid(game_name, tag_line)
 
     if puuid:
         last_match_id = load_last_match(game_name, tag_line)
-        recent_matches = get_recent_match_ids(puuid)
+        recent_match_ids = get_recent_match_ids(puuid)
+        
+        if recent_match_ids:
+            recent_match_id = recent_match_ids[0]
 
-        if recent_matches:
-            recent_match_id = recent_matches[0]
-
-            if recent_match_id != last_match_id:
+            if last_match_id != recent_match_id:
                 match_details = get_match_details(recent_match_id)
+
                 if match_details:
                     participant = next(p for p in match_details['info']['participants'] if p['puuid'] == puuid)
-                    win = participant['win']
-                    champion = participant['championName']
-                    kills = participant['kills']
-                    deaths = participant['deaths']
-                    assists = participant['assists']
-                    match_duration = match_details['info']['gameDuration']
-                    farm = participant['totalMinionsKilled']
-                    queue_id = match_details['info']['queueId']
-                    game_mode = QUEUE_ID_MAP.get(queue_id, "Unknown Mode")
+                    game_mode = QUEUE_ID_MAP.get(match_details['info']['queueId'], "Unknown")
 
-                    embed = Embed(title=f"New Match for {game_name}#{tag_line}", color=discord.Color.green() if win else discord.Color.red())
-                    embed.add_field(name="Game Mode", value=game_mode, inline=False)
-                    embed.add_field(name="Result", value="Victory" if win else "Defeat", inline=True)
-                    embed.add_field(name="Champion", value=champion, inline=True)
-                    embed.add_field(name="KDA", value=f"{kills}/{deaths}/{assists}", inline=True)
-                    embed.add_field(name="Duration", value=f"{match_duration // 60}m {match_duration % 60}s", inline=True)
-                    embed.add_field(name="Farm (CS)", value=str(farm), inline=True)
+                    if game_mode in ["Ranked Solo", "Ranked Flex"]:
+                        win_or_loss = "won" if participant['win'] else "lost"
+                        champ_name = participant['championName']
+                        score = f"{participant['kills']}/{participant['deaths']}/{participant['assists']}"
+                        
+                        # Create an embed for the message
+                        embed = discord.Embed(title=f"New Match for {game_name}#{tag_line}", color=discord.Color.green() if participant['win'] else discord.Color.red())
+                        embed.add_field(name="Game Mode", value=game_mode, inline=False)
+                        embed.add_field(name="Result", value="Victory" if participant['win'] else "Defeat", inline=True)
+                        embed.add_field(name="Champion", value=champ_name, inline=True)
+                        embed.add_field(name="KDA", value=score, inline=True)
+                        embed.add_field(name="Duration", value=f"{match_details['info']['gameDuration'] // 60}m {match_details['info']['gameDuration'] % 60}s", inline=True)
+                        embed.add_field(name="Farm (CS)", value=str(participant['totalMinionsKilled'] + participant['neutralMinionsKilled']), inline=True)
+                        
+                        await channel.send(embed=embed)
 
-                    await channel.send(embed=embed)
-
+                    # Save the new match ID
                     save_last_match(game_name, tag_line, recent_match_id)
-    else:
-        await channel.send(f"Unable to get PUUID for {game_name}#{tag_line}.")
+            else:
+                print(f"No new match for {game_name}#{tag_line}")
+        else:
+            print(f"No matches found for {game_name}#{tag_line}")
 
 @client.event
 async def on_ready():
